92ae1aae27bb1e2058019d47c9ee2e618a4e8ac7
srinja <srinja@example.com>
2026-05-17 14:53:02 +0530

    jenkins-docker-demo

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Dockerized Jenkins Demo!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
+
